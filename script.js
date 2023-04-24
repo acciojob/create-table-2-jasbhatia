96240f25552736a211eaa357505c9d8a3729ae74
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-04-24 21:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function createTable() {
     //Write your code 
-let rows= prompt("input number of rows");
-let cols= prompt("input number of cols");
+ rn = window.prompt("Input number of rows", 1);
+  cn = window.prompt("Input number of columns", 1);
 
-	for(let i=1;i<=rows;i++){
-		let x= document.getElementById("MyTable").insertRow(i);
-		for(let j=1;j<=cols;j++){
-			var y=x.insertCell(j);
-			y.innerText= "row" +i +","+ "col"+j;
-		}
-	}
-  
+  for (var r = 0; r < parseInt(rn, 10); r++) {
+    var x = document.getElementById("myTable").insertRow(r);
+    for (var c = 0; c < parseInt(cn, 10); c++) {
+      var y = x.insertCell(c);
+      y.innerHTML = "Row-" + r + " Column-" + c;
+    }
+  }
 }
