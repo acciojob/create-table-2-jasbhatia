55dabdf1da4167aacf1bae0ebe87188790c9034b
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-04-24 15:48:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ function createTable() {
     //Write your code 
 let rows= prompt("input number of rows");
 let cols= prompt("input number of cols");
+
+	for(let i=1;i<=rows;i++){
+		let x= document.getElementById("MyTable").insertRow(i);
+		for(let j=1;j<=cols;j++){
+			var y=x.insertCell(j);
+			y.innerText= "row" +i +","+ "col"+j;
+		}
+	}
   
 }
